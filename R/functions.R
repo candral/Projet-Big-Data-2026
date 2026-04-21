61950c0e8c377d765a90c1818caf67e6daf0bc0e
CLMHVT <clmhervouet@gmail.com>
2026-04-21 22:20:13 +0200

    feat: conversion coordonnées

--- R/functions.R
+++ R/functions.R
@@ -11,3 +11,18 @@ clean_users <- function(data) {
       TRUE ~ created_user
     ))
 }
+
+# Conversion coordonnées
+convert_coords <- function(data) {
+  data_sf <- data %>%
+    filter(!is.na(X), !is.na(Y)) %>%
+    st_as_sf(coords = c("X", "Y"), crs = 3949) %>%
+    st_transform(crs = 4326)
+  
+  coords <- st_coordinates(data_sf)
+  data_sf$long <- coords[, 1]
+  data_sf$lat <- coords[, 2]
+  
+  return(as.data.frame(data_sf) %>%
+           select(-geometry))
+}
--- main.r
+++ main.r
@@ -7,4 +7,5 @@ source("R/functions.R")
 df <- read.csv("data/Patrimoine_Arboré_data.csv")
 
 df_final <- df %>%
-  clean_users()
+  clean_users() %>%
+  convert_coords()
