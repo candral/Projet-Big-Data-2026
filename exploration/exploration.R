ce0d95c7e66ab5a075c065e58c7f503c0bc90c56
ClementHVT <clmhervouet@gmail.com>
2026-04-22 11:37:06 +0200

    feat: exploration & analyse

---
+++ exploration/exploration.R
@@ -0,0 +1,72 @@
+library(tidyverse)
+library(lubridate)
+
+df <- read.csv("data/Patrimoine_Arboré_data.csv", na.strings = c("", "NA"))
+
+# Structure et Types des données
+str(df)
+
+# Conversion des colonnes de dates (format YYYY/MM/DD)
+df$created_date <- ymd_hms(df$created_date)
+df$dte_plantation <- ymd_hms(df$dte_plantation)
+
+# Conversion des variables qualitatives en facteurs
+qualitative_vars <- c("fk_arb_etat", "fk_stadedev", "fk_port", "clc_quartier", "feuillage", "remarquable")
+df[qualitative_vars] <- lapply(df[qualitative_vars], as.factor)
+
+
+
+# Statistiques descriptives (Moyennes et Écarts-types)
+
+stats_quantitatives <- df %>%
+  select(haut_tot, haut_tronc, tronc_diam, age_estim) %>%
+  summarise(across(everything(), list(
+    moyenne = ~mean(., na.rm = TRUE),
+    ecart_type = ~sd(., na.rm = TRUE)
+  )))
+
+print(stats_quantitatives)
+
+# Histogramme de la distribution de la hauteur totale des arbres
+ggplot(df, aes(x = haut_tot)) +
+  geom_histogram(binwidth = 2, fill = "#2e7d32", color = "white") +
+  stat_bin(binwidth = 2, geom = "text", aes(label = after_stat(count)), 
+           vjust = -0.5, size = 3) +
+  labs(title = "Distribution de la hauteur totale des arbres",
+       x = "Hauteur totale (m)",
+       y = "Nombre d'arbres") +
+  theme_minimal()
+
+# Camembert sur la Répartition du stade de développement (fk_stadedev)
+pie_data <- df %>%
+  filter(!is.na(fk_stadedev)) %>%
+  count(fk_stadedev) %>%
+  mutate(prop = n / sum(n) * 100,
+         label = paste0(round(prop, 1), "%")) # Crée le texte du label
+
+ggplot(pie_data, aes(x = "", y = n, fill = fk_stadedev)) +
+  geom_bar(stat = "identity", width = 1) +
+  coord_polar("y", start = 0) +
+  geom_text(aes(label = label), 
+            position = position_stack(vjust = 0.5), # Centre le texte dans les parts
+            size = 4) +
+  labs(title = "Répartition par stade de développement",
+       fill = "Stade") +
+  theme_void()
+
+# Diagramme en barres
+bar_data <- df %>%
+  filter(!is.na(clc_quartier)) %>%
+  count(clc_quartier)
+
+ggplot(bar_data, aes(x = reorder(clc_quartier, n), y = n, fill = clc_quartier)) +
+  geom_bar(stat = "identity") +
+  geom_text(aes(label = n), 
+            hjust = -0.2, # Décale le texte juste après la fin de la barre
+            size = 3.5) +
+  coord_flip() +
+  labs(title = "Nombre d'arbres par quartier",
+       x = "Quartier",
+       y = "Nombre d'arbres") +
+  theme_minimal() +
+  theme(legend.position = "none")
