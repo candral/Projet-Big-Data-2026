d81a95e64635b7916801b2c84c8472b1552ebd63
clarisse <clarisse.andral@isen-ouest.yncrea.fr>
2026-04-27 14:41:13 +0200

    Feat : meilleur modèle

--- IA/Besoin 2/script.py
+++ IA/Besoin 2/script.py
@@ -5,7 +5,7 @@ import os
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
-def charger_modeles(modele_path='model/best_random_forest_model.pkl', scaler_path='standard_scaler.pkl'):
+def charger_modeles(modele_path='model/meilleur_modele_arbre.pkl', scaler_path='model/standard_scaler.pkl'):
     """Charge le modèle et le scaler enregistrés."""
     if not os.path.exists(modele_path) or not os.path.exists(scaler_path):
         raise FileNotFoundError("Erreur : Les fichiers modèles sont introuvables.")
@@ -15,27 +15,39 @@ def charger_modeles(modele_path='model/best_random_forest_model.pkl', scaler_pat
     return model, scaler
 
 def predire_age():
-    print("--- Outil de Prédiction (Unités : Centimètres) ---")
+    mapping_especes =  {0: 'Autres', 1: 'Bouleau', 2: 'Charme', 3: 'Chêne', 4: 'Hêtre', 5: 'Pin', 6: 'Platane', 7: 'Prunus', 8: 'Tilleul', 9: 'Érable'}
+
+    print("Outil de Prédiction (en centimètres):")
     
     try:
         model, scaler = charger_modeles()
+
+        print("\n Table des Espèces :")
+        for code, nom in mapping_especes.items():
+            print(f"[{code}] : {nom}")
+        print("-----------------")
         
-        print("\nVeuillez entrer les dimensions en CENTIMÈTRES :")
-        # On garde les noms de colonnes exacts de ton dataframe original
+        # Entrées utilisateur
+        genre_id = int(input("\nEntrez le code de l'espèce : "))
+        if genre_id not in mapping_especes:
+            print("Code espèce inconnu, utilisation du code 0 (Autres).")
+            genre_id = 0
+
+        # On garde les noms de colonnes exacts du datafrale
         haut_tot = float(input("Hauteur totale (cm) : "))
         haut_tronc = float(input("Hauteur du tronc (cm) : "))
         tronc_diam = float(input("Diamètre du tronc (cm) : "))
         
         # Création du DataFrame avec les mêmes noms de colonnes que lors du training
-        input_data = pd.DataFrame([[haut_tot, haut_tronc, tronc_diam]], 
-                                 columns=['haut_tot', 'haut_tronc', 'tronc_diam'])
-        
-        # Le scaler va normaliser ces valeurs en cm comme il l'a fait pour le train_set
+        input_data = pd.DataFrame([[haut_tot, haut_tronc, tronc_diam, genre_id]], 
+                                 columns=['haut_tot', 'haut_tronc', 'tronc_diam', 'genre_id'])
+        # Scaler normalise les valeurs en cm
         input_scaled = scaler.transform(input_data)
         
         age_predit = model.predict(input_scaled)[0]
         
         print("-" * 45)
+        print(f"Espèce : {mapping_especes[genre_id]}")
         print(f"Résultat : L'âge estimé est de {age_predit:.1f} ans.")
         print("-" * 45)
 
